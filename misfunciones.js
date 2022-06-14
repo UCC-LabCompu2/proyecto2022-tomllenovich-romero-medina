df51137554c6b93a3fb2b499ce9c46994b516e8d
jerekiler845 <2107117@ucc.edu.ar>
2022-06-13 23:36:12 -0300

    se creo javascrip archivo

---
+++ misfunciones.js
@@ -0,0 +1,11 @@
+function iniciojuego(){
+    window.open("index%20juego.html");
+}
+
+function nombrecomprobar(id){
+    if (isNaN(id)){
+
+    }else{
+        alert("el nombre ingresado no es valido")
+    }
+}
